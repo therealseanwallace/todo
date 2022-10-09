d5c532825c24902b0fcb3e63414d227f062a6349
Sean Wallace <wallace.sean111@gmail.com>
2022-10-09 02:26:39 +0200

    Add event listeners

--- src/buildPage.js
+++ src/buildPage.js
@@ -1,10 +1,14 @@
 import { toDoFactory, tasks } from './appLogic';
 
 const builder = (element, parent, classes, type, value, text) => {
+  // Builds and appends DOM nodes according to supplied arguments
   const selectParent = document.querySelector(parent);
   const makeComponent = document.createElement(element);
   if (classes !== undefined) {
     makeComponent.classList.add(classes);
+    if (classes === 'tab' || classes === 'new-task-btn') {
+      makeComponent.classList.add('button');
+    }
   }
   if (type !== undefined) {
     makeComponent.type = type;
@@ -19,6 +23,7 @@ const builder = (element, parent, classes, type, value, text) => {
 };
 
 const buildTasks = () => {
+  // Creates cards corresponding to each task from taskArray
   const test = (() => {
     const test = toDoFactory('test', 'this is a test', 'NOW!', 'IMPORTANT!');
     tasks.taskArray.push(test);
@@ -26,21 +31,27 @@ const buildTasks = () => {
   })();
   console.log('Building tasks!');
   for (let i = 0; i < tasks.taskArray.length; i += 1) {
-    console.log('for loop active');
-    const newTaskCard = builder('div', '.task-display', `task-card-${i}`);
-    const taskCardH3 = builder('h3', `.task-card-${i}`, `task-${i}-title`, undefined, undefined, tasks.taskArray[i].title);
-    const taskDue = builder('p', `.task-card-${i}`, `task-${i}-due`, undefined, undefined, tasks.taskArray[i].dueDate);
+    builder('div', '.task-display', `task-card-${i}`);
+    builder('h3', `.task-card-${i}`, `task-${i}-title`, undefined, undefined, tasks.taskArray[i].title);
+    builder('p', `.task-card-${i}`, `task-${i}-due`, undefined, undefined, tasks.taskArray[i].dueDate);
   }
 };
 
 const buildUI = (() => {
   builder('menu', '#content', 'tabs');
-  builder('input', '.tabs', undefined, 'button', 'button', 'Tasks');
-  builder('input', '.tabs', undefined, 'button', 'button', 'Projects');
+  builder('input', '.tabs', 'tab', 'button', 'Tasks');
+  builder('input', '.tabs', 'tab', 'button', 'Projects');
   builder('main', '#content');
   builder('input', 'main', 'new-task-btn', 'button', 'New Task');
   builder('div', 'main', 'task-display');
   buildTasks();
+  const buttons = document.querySelectorAll('.button');
+  function working() {
+    console.log('This event listener works');
+  }
+  for (let i = 0; i < buttons.length; i += 1) {
+    buttons[i].addEventListener('click', working);
+  }
 })();
 
 export {
