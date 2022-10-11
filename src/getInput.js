3813030ee0213bf500a76f3e27f691a69a99a1f4
Sean Wallace <wallace.sean111@gmail.com>
2022-10-11 17:14:49 +0200

    Fix adding new tasks and dropdowns

--- src/getInput.js
+++ src/getInput.js
@@ -5,9 +5,12 @@ export { addListeners };
 
 let tempObject = taskFactory();
 tempObject.altered = false;
+tempObject.project = 0;
 
 const addListeners = () => {
+  console.log('Adding listeners!')
   const inputs = document.querySelectorAll('input');
+  console.log('inputs is', inputs);
   for (let i = 0; i < inputs.length; i += 1) {
     if (inputs[i].classList.contains('button')) {
       inputs[i].addEventListener('click', getMouseInput);
@@ -16,13 +19,22 @@ const addListeners = () => {
     }
   }
   // Checks if the project selector has been drawn. If so, adds event listener.
-  if (document.querySelector('.project-select') !== null) {
-    document.querySelector('.project-select').addEventListener('change', getMouseInput);
+  const dropdowns = document.querySelectorAll('select');
+  console.log('dropdowns is', dropdowns);
+  for (let i = 0; i < dropdowns.length; i++) {
+    console.log('adding listeners to dropdowns');
+    const element = dropdowns[i];
+    element.addEventListener('change', getMouseInput);
   }
+  
+  /*if (document.querySelector('.project-select') !== null) {
+    document.querySelector('.project-select').addEventListener('change', getMouseInput);
+  }*/
 };
 
 const getMouseInput = (e) => {
   //console.log(e.target.value);
+  console.log('getting mouse input!')
   if (e.target.id === 'new-task') {
     newTask();
     addListeners();
@@ -31,18 +43,21 @@ const getMouseInput = (e) => {
     tempObject.priority = e.target.value;
     tempObject.altered = true;
   }
-  
-  if (e.target.id === 'project-select') {
-    tempObject.project = e.target.options[e.target.selectedIndex].value;
+   
+  if (e.target.classList.contains('project-select')) {
+    tempObject.project = e.target.selectedIndex;
     tempObject.altered = true;
+    console.log('tempObject is', tempObject);
   }
 
   // Adds the new task to the appropriate array as defined by the second parameter
   if (e.target.value === 'Submit') {
+    console.log('tempObject.project is', tempObject.project);
     tasks.addTask(tempObject, tempObject.project);
     console.log(tempObject);
     tempObject = taskFactory();
     tempObject.altered = false;
+    tempObject.project = 0;
     console.log(tempObject);
     buildTasks();
   };
