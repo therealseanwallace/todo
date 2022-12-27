5a5cca6b2c6d1528f0f0b77fdea0573e2cb868a6
Sean Wallace <wallace.sean111@gmail.com>
2022-12-27 06:46:32 +0100

    Add functionality to task card due dates #5

--- src/components/taskCards/taskCardDueDate.js
+++ src/components/taskCards/taskCardDueDate.js
@@ -2,21 +2,20 @@ import React, { Component } from "react";
 import { makeClassName, makeID } from "../../helpers/makeClassName";
 
 class TaskCardDueDate extends Component {
-  constructor(props) { 
+  constructor(props) {
     super(props);
   }
   render() {
     return (
       <div>
         <input
-           className="task-card-due-date"
-           id = {makeID("due-date", this.props.task.taskID)}
-           data-task-id={this.props.task.taskID}
-           defaultValue={this.props.task.dueDate}
-           onChange={this.props.onChange}
-           type="date"
+          className="task-card-due-date"
+          id={makeID("due-date", this.props.task.taskID)}
+          data-task-id={this.props.task.taskID}
+          defaultValue={this.props.task.dueDate}
+          onChange={this.props.onChange}
+          type="date"
         />
-
       </div>
     );
   }
