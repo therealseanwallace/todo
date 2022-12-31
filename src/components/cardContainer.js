eeb66d01b089cc86832c00a001c915a328ea0d13
Sean Wallace <wallace.sean111@gmail.com>
2022-12-31 15:22:26 +0100

    Polish styling #12

--- src/components/cardContainer.js
+++ src/components/cardContainer.js
@@ -20,26 +20,31 @@ class CardContainer extends Component {
     return (
       <div className="card-container">
         <h1>Tasks</h1>
-        {this.props.tasks.tasks.map((task) => {
-          const key = task.taskID;
-          if (!task.isProject) {
-            // if the task corresponds to the selected project,
-            // render it as a task card
-            if (task.parent === this.props.tasks.selectedProject && !task.isDeleted) {
-              return (
-                <div key={key}>
-                  <TaskCard
-                    completeTask={this.props.completeTask}
-                    task={task}
-                    onChange={this.props.onChange}
-                    tasklist={this.props.tasks}
-                    deleteTask={this.props.deleteTask}
-                  />
-                </div>
-              );
+        <div className={"card-container-inner"}>
+          {this.props.tasks.tasks.map((task) => {
+            const key = task.taskID;
+            if (!task.isProject) {
+              // if the task corresponds to the selected project,
+              // render it as a task card
+              if (
+                task.parent === this.props.tasks.selectedProject &&
+                !task.isDeleted
+              ) {
+                return (
+                  <div key={key}>
+                    <TaskCard
+                      completeTask={this.props.completeTask}
+                      task={task}
+                      onChange={this.props.onChange}
+                      tasklist={this.props.tasks}
+                      deleteTask={this.props.deleteTask}
+                    />
+                  </div>
+                );
+              }
             }
-          }
-        })}
+          })}
+        </div>
       </div>
     );
   }
