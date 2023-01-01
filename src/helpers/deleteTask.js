0ca981141a4056eca60c4455c0018b5147ab6fff
Sean Wallace <wallace.sean111@gmail.com>
2023-01-01 11:27:26 +0100

    Clean code

--- src/helpers/deleteTask.js
+++ src/helpers/deleteTask.js
@@ -1,16 +1,13 @@
 function deleteTask(taskID) {
-  console.log("deleteTask! taskID is: ", taskID);
-  console.log("Number(taskID) is: ", Number(taskID));
   const taskIDNum = Number(taskID);
   const taskArray = this.state.tasks;
   if (taskIDNum !== 0) {
-    console.log("taskID is not 0, deleting");
     const taskIndex = this.getTaskIndexByID(Number(taskID));
     const task = taskArray[taskIndex];
-    console.log("task is: ", task);
+
     task.isDeleted = true;
     this.setState({ ...this.state, tasks: taskArray });
-    
+
     if (task.isProject) {
       taskArray.forEach((item) => {
         if (item.parent === task.taskID) {
@@ -19,10 +16,7 @@ function deleteTask(taskID) {
         }
       });
     }
-  } else {
-    console.log("taskID is 0, not deleting");
   }
-  console.log("deleteTask complete. this.state is: ", this.state);
-};
+}
 
 export default deleteTask;
